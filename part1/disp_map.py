fb462de320c2d9fd27215df233a4cfaceaef6c76
AirRunner <luca.vaio@efrei.net>
2022-03-24 00:29:47 -0400

    Display multiple map animation for

--- part1/disp_map.py
+++ part1/disp_map.py
@@ -21,79 +21,93 @@ info_text.set("Carte:\nCoût:\nProbabilité du but:")
 label = Label(window, textvariable=info_text, font=("SF Mono", 27), justify=LEFT)
 label.pack()
 
-# Load map
-map = "Caldera"
-world = World(map)
-probabilities, real_goals = list(), list()
+for map in ("Enigma", "WinterConquest", "BigGameHunters", "Caldera"):
+    # Load map
+    world = World(map)
+    probabilities, real_goals = list(), list()
 
-# Predict for each observation
-for i in range(len(world.observations)):
-    iter_text.set(str(i))
-    
-    # Compute goal probability
-    probs = world.predictMastersSardina(world.observations[i])
-    pred_goal = argmax(probs)
-    
-    probabilities.append(probs)
-    real_goals.append(world.goals[argmax(world.labels[i])])
+    # Predict for each observation
+    for i in range(len(world.observations)):
+        iter_text.set(str(i))
+        
+        # Compute goal probability
+        probs = world.predictMastersSardina(world.observations[i])
+        pred_goal = argmax(probs)
+        
+        probabilities.append(probs)
+        real_goals.append(world.goals[argmax(world.labels[i])])
 
-    # Generate map image
-    img = PhotoImage(width=WIDTH, height=HEIGHT)
+        correct = argmax(probs) == argmax(world.labels[i])
 
-    for x, y in product(range(WIDTH), range(HEIGHT)):
-        if world.grid[x][y] is not None:
-            img.put("#FFFFFF", (x, y))
-    
-    # Display start and current positions
-    img.put("#FF2600", tuple(world.observations[i].point))
-    img.put("#FF9300", tuple(world.start.point))
-    
-    # Display goals
-    for goal in world.goals:
-        img.put("#0096FF", tuple(goal.point))
-    
-    # Display path to goal
-    path_s_p = world.planner(world.start, world.observations[i])
-    path_s_g = world.planner(world.start, world.goals[pred_goal])
-    path_p_g = world.planner(world.observations[i], world.goals[pred_goal])
+        # Generate map image
+        img = PhotoImage(width=WIDTH, height=HEIGHT)
 
-    for node in path_s_p[1:-1]:
-        img.put("#9EC591", tuple(node.point))
-    for node in path_p_g[1:-1]:
-        img.put("#008F00", tuple(node.point))
-    
-    # Display general informations
-    s_p = "Départ → Position (coût G)"
-    p_g = "Position → But (coût H)"
-    s_g = "Départ → But"
-    prob = "Probabilité du but"
-    soleil = "soleil"
-    max_w = len(s_p) + 1
+        for x, y in product(range(WIDTH), range(HEIGHT)):
+            if world.grid[x][y] is not None:
+                img.put("#FFFFFF", (x, y))
+        
+        # Display start and current positions
+        img.put("#FF2600", tuple(world.observations[i].point))
+        img.put("#FF9300", tuple(world.start.point))
+        
+        # Display goals
+        for goal in world.goals:
+            img.put("#0096FF", tuple(goal.point))
+        
+        # Display path to goal
+        path_s_p = world.planner(world.start, world.observations[i])
+        path_s_g = world.planner(world.start, world.goals[pred_goal])
+        path_p_g = world.planner(world.observations[i], world.goals[pred_goal])
 
-    info_text.set(f"\n——  Carte {map}  ——\n\
+        for node in path_s_p[1:-1]:
+            img.put("#9EC591", tuple(node.point))
+        for node in path_p_g[1:-1]:
+            img.put("#008F00", tuple(node.point))
+        
+        # Display general informations
+        s_p = "Départ → Position"
+        p_g = "Position → But"
+        s_g = "Départ → But"
+        prob = "Probabilité du but"
+        max_w = len(s_p) + 1
+
+        info_text.set(f"\n——  Carte {map}  ——\n\
 {s_p:{max_w}} {len(path_s_p): >{5}}\n\
 {p_g:{max_w}} {len(path_p_g): >{5}}\n\
 {s_g:{max_w}} {len(path_s_g): >{5}}\n\
 {prob:{max_w}} {str(max(probs))[:4]: >{5}}\
 \n")
 
-    # Create image and text
-    img = img.zoom(10, 10)
-    canvas.delete("all")
-    canvas.create_image((WIDTH * 5, HEIGHT * 5), image=img, state="normal")
-    canvas.create_rectangle(10, 10, 90, 50, fill='#424242', width=0)
-    canvas.create_text((50, 31), text=str(i + 1), fill="#FF9800", font=custom_font)
+        # Create image and text
+        img = img.zoom(10, 10)
+        canvas.delete("all")
+        
+        canvas.create_image((WIDTH * 5, HEIGHT * 5), image=img, state="normal")
+        
+        canvas.create_rectangle(10, 10, 90, 50, fill='#424242', width=0)
+        canvas.create_text((50, 31), text=str(i + 1), fill="#FF9800", font=custom_font)
+        
+        canvas.create_rectangle(590, 10, 630, 50, fill='#424242', width=0)
+        canvas.create_text(
+            (608, 30),
+            text='✓' if correct else '✗',
+            fill="#00FF00" if correct else "#FF0000",
+            font=custom_font
+        )
+
+        window.update()
+        # sleep(.5)
+
+    accuracy = world.accuracy(probabilities, real_goals)
+    print(f"Global accuracy for this map: {accuracy}%")
+
+    canvas.create_rectangle(140, 220, 500, 420, fill='#424242', width=0)
+    canvas.create_text(
+        (320, 320), text=f"Justesse globale\npour cette carte :\n\n{accuracy}% !",
+        fill="#FF9800", font=custom_font, justify=CENTER
+    )
 
     window.update()
-    sleep(.5)
-
-accuracy = world.accuracy(probabilities, real_goals)
-print(f"Global accuracy for this map: {accuracy}%")
-
-canvas.create_rectangle(140, 220, 500, 420, fill='#424242', width=0)
-canvas.create_text(
-    (320, 320), text=f"Justesse globale\npour cette carte :\n\n{accuracy}% !",
-    fill="#FF9800", font=custom_font, justify=CENTER
-)
+    sleep(2)
 
 mainloop()
